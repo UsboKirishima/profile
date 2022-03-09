1bccd02675463e4b79d3987b9b8329b3a9e03c42
UsboKirishima <usbertibox@gmail.com>
2022-03-09 22:18:36 +0100

    feat: Home page

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = ({
+    active
+}: {
+    active: string;
+}) => {
+    return(
+        <div></div>
+    );
+}
---
+++ pages/Home.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+    return (
+        <>
+        <Wrapper>
+            
+        </Wrapper>
+        </>
+    );
+}
+
+const Wrapper = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 100vh;
+color: #C3BAC6;
+`;
+
+
+export default Home;
