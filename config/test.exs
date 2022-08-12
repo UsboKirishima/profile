c7b03f56c3903dc5fc8a8c4169ab155480ac6917
UsboKirishima <usbertibox@gmail.com>
2022-08-12 23:51:38 +0200

    feat(*): removed module docs

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :profile, Profile.Repo,
   username: "postgres",
   password: "postgres",
@@ -13,18 +8,13 @@ config :profile, Profile.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :profile, ProfileWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "nenh8CSxAOG4FDQbC+j+1Au1oTVf5Aosgx5JFW7jssLfV0ui5RnA+E23OYbHuIG1",
   server: false
 
-# In test we don't send emails.
 config :profile, Profile.Mailer, adapter: Swoosh.Adapters.Test
 
-# Print only warnings and errors during test
 config :logger, level: :warn
 
-# Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
--- lib/profile_web.ex
+++ lib/profile_web.ex
@@ -1,21 +1,4 @@
 defmodule ProfileWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use ProfileWeb, :controller
-      use ProfileWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
 
   def controller do
     quote do
@@ -33,11 +16,9 @@ defmodule ProfileWeb do
         root: "lib/profile_web/templates",
         namespace: ProfileWeb
 
-      # Import convenience functions from controllers
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
-      # Include shared imports and aliases for views
       unquote(view_helpers())
     end
   end
@@ -86,13 +67,10 @@ defmodule ProfileWeb do
 
   defp view_helpers do
     quote do
-      # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
-      # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
 
-      # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
 
       import ProfileWeb.ErrorHelpers
@@ -101,9 +79,7 @@ defmodule ProfileWeb do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
+
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
