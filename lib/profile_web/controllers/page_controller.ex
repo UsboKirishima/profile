84db291cb4150ed4178f31f7d4f202cf5bf10759
UsboKirishima <usbertibox@gmail.com>
2022-08-21 13:21:58 +0200

    feat(*): font restyle & responsive text

--- lib/profile_web/controllers/page_controller.ex
+++ lib/profile_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule ProfileWeb.PageController do
   use ProfileWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html", description: "I'm Davide, also called by friends and strangers 'usbo', I born in a little city near Parma in Italy, I'm studying Automation and Robotic at high school. My dream future is to work as backend developer at Discord, I'll move to Switzerland after the end of the school. I started to coding websites and APIs few years ago using Node.js frameworks, now I'm trying to learn new frameworks with Elixir.")
+    render(conn, "index.html", description: "I'm Davide, also called by friends and strangers 'usbo', I born in a little city near Parma in Italy, I'm studying Automation and Robotic at high school. My dream future is to work as backend developer at Discord, I'll move to Switzerland after the end of the school. I started to coding websites and APIs few years ago using Node.js frameworks, now I'm trying to learn more about Elixir frameworks.")
   end
 
 
