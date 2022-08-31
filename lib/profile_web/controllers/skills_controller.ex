9ba262804e84e7a54d3a2128f76e3fa1bf9039b0
UsboKirishima <usbertibox@gmail.com>
2022-09-01 00:23:50 +0200

    feat(skills): add skills page

--- lib/profile_web/controllers/skills_controller.ex
+++ lib/profile_web/controllers/skills_controller.ex
@@ -2,6 +2,26 @@ defmodule ProfileWeb.SkillsController do
   use ProfileWeb, :controller
 
   def skills(conn, _params) do
-    render(conn, "skills.html")
+    render(conn,
+    "skills.html",
+    languages: [
+      "TypeScript",
+      "Elixir",
+      "C",
+      "Rust",
+      "Haskell"
+    ],
+    frameworks: [
+      "Phoenix",
+      "Node.js",
+      "Next.js",
+      "GTK+"
+    ],
+    tools: [
+      "Neovim",
+      "Alacritty",
+      "Arch Linux"
+    ]
+    )
   end
 end
