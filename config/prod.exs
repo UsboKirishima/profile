07425ad52eb857c2c8fc546c1df8fb242694ecfd
UsboKirishima <usbertibox@gmail.com>
2022-09-01 11:33:56 +0200

    feat(config): set ssl true

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,9 @@
 import Config
 
 
-config :profile, ProfileWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
+config :profile, ProfileWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json",  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 config :logger, level: :info
+
+config :profile, Profile.Repo,
+  ssl: true
